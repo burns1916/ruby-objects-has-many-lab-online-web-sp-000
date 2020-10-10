c20620e8afb5d167902d1ce28c4f9e4c278b3d27
Ryan K Burns <ryankburns@gmail.com>
2020-10-10 15:47:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,21 @@ class Artist
   attr_accessor :name,
 
   def initialize(name)
-
+    @name = name
   end
 
+  def add_song(song)
+    song.artist = self
+  end
 
+  def add song_by_name(song_name)
+    song = Song.new(name)
+    add_song(song)
+  end
 
+  def self.song_count
+    @@all.length
+  end
+  
+  
 end
